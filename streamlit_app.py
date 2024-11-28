58c18cca9d3b7572c53345a6fe21a1afea0037b1
santosh kumar asapu <santoshasapu1998@gmail.com>
2024-11-28 12:49:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,13 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
-LOGO_URL_SMALL=firstStreamlitapp/logo.png
+LOGO_URL_SMALL="firstStreamlitapp/logo.png"
+LOGO_URL_LARGE="firstStreamlitapp/cvs healh logo.jfif"
 
 st.logo(
-    LOGO_URL_SMALL,
+    LOGO_URL_LARGE,
     link="https:https://www.cvshealth.com/",
+    icon_image=LOGO_URL_SMALL,
     
 )
 st.title(' Machine Learning App')
