255d5cce71b0c21d0f6665424c8eb36e0edcfb75
santosh kumar asapu <santoshasapu1998@gmail.com>
2024-11-28 12:11:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
-st.title('🤖 Machine Learning App')
+st.title(' Machine Learning App')
 
 st.info('This is app builds a machine learning model!')
 
