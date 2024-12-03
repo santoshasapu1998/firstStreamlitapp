3a48ef84ecafc1d59fade5581b009f9b228a32dd
santosh kumar asapu <santoshasapu1998@gmail.com>
2024-12-03 10:26:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,13 @@
 import streamlit as st
 
-st.logo(
-    LOGO_URL_LARGE,
-    link="https://streamlit.io/gallery",
-    icon_image=LOGO_URL_SMALL,
-)
+HORIZONTAL_RED = "firstStreamlitapp/cvs health logo (1).png"
+ICON_RED = "firstStreamlitapp/cvs health logo (1).png"
+HORIZONTAL_BLUE = "firstStreamlitapp/cvs health logo (1).png"
+ICON_BLUE = "firstStreamlitapp/cvs health logo (1).png"
+
+options = [HORIZONTAL_RED, ICON_RED, HORIZONTAL_BLUE, ICON_BLUE]
+sidebar_logo = st.selectbox("Sidebar logo", options, 0)
+main_body_logo = st.selectbox("Main body logo", options, 1)
+
+st.logo(sidebar_logo, icon_image=main_body_logo)
+st.sidebar.markdown("Hi!")
